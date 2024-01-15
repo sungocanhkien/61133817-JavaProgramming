a0f2afffbfb3ea487c3d2e76433a75b32ecabb6f
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-01-16 04:55:55 +0700

    ví dụ đầu tiên
    chỉ ví dụ

---
+++ HelloWorld/src/still/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package still.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao");
+	}
+
+}

5ad7aa266bae90f631850cf63dde3114326307b1
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-06 23:11:56 +0700

    biến Java
    cách khai báo biến

---
+++ HelloWorld/src/still/edu/Bien.java
@@ -0,0 +1,14 @@
+package still.edu;
+
+public class Bien {
+
+	public static void main(String[] args) {
+		String thongBao = "Xin chào";
+		String hoTen = "Anh Kien";
+		String tenHocPhan = "Java";
+		
+		System.out.println("Thông báo: "+ thongBao);
+		System.out.println("Họ tên: "+ hoTen);
+		System.out.println("Tên học phần: "+ tenHocPhan);
+	}
+}

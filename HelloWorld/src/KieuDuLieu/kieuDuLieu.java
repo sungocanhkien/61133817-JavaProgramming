660b069378641207ea3fd7c911bd0988bafe4b27
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-06 23:21:47 +0700

    Kiểu dữ liệu
    làm quen khai báo kiểu dữ liệu trong java

---
+++ HelloWorld/src/KieuDuLieu/kieuDuLieu.java
@@ -0,0 +1,11 @@
+package KieuDuLieu;
+
+public class kieuDuLieu {
+	public static void main(String[] args) {
+		String tenSach = "Lập trình Java";
+		int namXuatBan = 2023;
+		double giaTien = 20.5;
+		boolean conSach = true;
+		char maKho = 'M';
+	}
+}

b4d43b79b88cbc700ed7400985ce924edbb247f9
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-07 01:33:11 +0700

    phép toán một ngôi
    thử một số phép toán một ngôi

---
+++ HelloWorld/src/ToanMotNgoi/ToanMotNgoi.java
@@ -0,0 +1,22 @@
+package ToanMotNgoi;
+
+public class ToanMotNgoi {
+	public static void main(String[] args) {
+		int a = 5;
+		boolean b = false;
+		System.out.println(" -a => giá trị: "+ (-a));
+		System.out.println(" +a => giá trị: "+ (+a));
+		System.out.println(" !b => giá trị: "+ (!b));
+		
+		System.out.println("------------");
+		System.out.println("a = "+ a);
+		System.out.println("++a = "+ (++a));
+		System.out.println("++a = "+ (a++));
+		
+		System.out.println("a = "+ a);
+		System.out.println("--a = "+ (--a));
+		System.out.println("a-- = "+ (a--));
+		
+		System.out.println("a = "+ a);
+	}
+}

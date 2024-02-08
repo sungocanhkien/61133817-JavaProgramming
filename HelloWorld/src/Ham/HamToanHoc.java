a6dfb45c5fa5e78506bda0212d31d12821ed456d
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-08 18:18:45 +0700

    Hàm
    ôn một số hàm toán học trong java

---
+++ HelloWorld/src/Ham/HamToanHoc.java
@@ -0,0 +1,36 @@
+package Ham;
+
+import java.util.Scanner;
+
+public class HamToanHoc {
+	public static void main(String[] args) {
+		double a, b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập vào số a: ");
+		a = sc.nextDouble();
+		System.out.println("Nhập vào số b: ");
+		b = sc.nextDouble();
+		
+		//Hàm trị tuyệt đối
+		System.out.println("|a|=" + Math.abs(a));
+		
+		// Hàm tìm min
+		System.out.println("min(a, b) = " + Math.min(a, b));
+		
+		// Hàm tìm Max
+		System.out.println("max(a, b) = " + Math.max(a, b));
+		
+		// Hàm ceil
+		System.out.println("ceil(a) = " + Math.ceil(a));
+		
+		// Hàm floor
+		System.out.println("floor(a) = " + Math.floor(a);
+		
+		// Hàm căn bậc hai
+		System.out.println("sqrt(a) = " + Math.sqrt(a));
+		
+		
+	
+		
+	}
+}

cb71034bf27c6e93798c3d937a2afbc8ae2e0ce7
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-08 23:12:23 +0700

    Update PhuongTrinhBacI.java

--- HelloWorld/src/BTChanLe/PhuongTrinhBacI.java
+++ HelloWorld/src/BTChanLe/PhuongTrinhBacI.java
@@ -1,5 +1,30 @@
 package BTChanLe;
 
-public class PhuongTrinhBacI {
+import java.util.Scanner;
 
+public class PhuongTrinhBacI {
+	public static void main(String[] args) {
+		double a, b, x;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập a = ");
+		a = sc.nextDouble();
+		System.out.println("Nhập b = ");
+		b = sc.nextDouble();
+		
+		if(a == 0)
+		{
+			if(b==0)
+			{
+				System.out.println("Phương trình vô số nghiệm.");
+			}else
+			{
+				System.out.println("Phương trình vô nghiệm.");
+			}
+		}
+		else
+		{
+			x = -b/a;
+			System.out.println("Phương trình có nghiệm: x = "+ x);
+		}
+	}
 }

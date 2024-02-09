989e95c99b2037a478c3b9a64988bb84c58046a8
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-09 15:38:49 +0700

    Update BTchepPhat.java

--- HelloWorld/src/VongLapFor/BTchepPhat.java
+++ HelloWorld/src/VongLapFor/BTchepPhat.java
@@ -1,5 +1,10 @@
 package VongLapFor;
 
 public class BTchepPhat {
-
+	public static void main(String[] args) {
+		for (int i = 0; i <= 20; i++) {
+			System.out.println(i+ ". Luyện tập code thường xuyên.");
+			
+		}
+	}
 }

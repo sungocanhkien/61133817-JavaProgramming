fce4866f4c23a7d08881acf3896858a743e5478d
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-09 15:56:28 +0700

    Update BTvongLapWhile.java

--- HelloWorld/src/VongLapWhile/BTvongLapWhile.java
+++ HelloWorld/src/VongLapWhile/BTvongLapWhile.java
@@ -11,5 +11,13 @@ public class BTvongLapWhile {
 			System.out.println("Nhập x = 0 để thoát; Nhập ký tự khác để tiếp tục: ");
 			x = sc.nextInt();
 		}
+		int i = 0;
+		while(true)
+		{
+			System.out.println(i);
+			i++;
+			if(i==10)
+				break;
+		}
 	}
 }

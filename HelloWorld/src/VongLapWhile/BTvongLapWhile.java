683803e1ece944fec02b96a9717f1a963f5edbf4
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-09 15:53:05 +0700

    Vòng lặp while
    làm bài tập ôn lại vòng lặp while trong java

---
+++ HelloWorld/src/VongLapWhile/BTvongLapWhile.java
@@ -0,0 +1,15 @@
+package VongLapWhile;
+
+import java.util.Scanner;
+
+public class BTvongLapWhile {
+	public static void main(String[] args) {
+		int x =1;
+		Scanner sc = new Scanner(System.in);
+		while(x!=0)
+		{
+			System.out.println("Nhập x = 0 để thoát; Nhập ký tự khác để tiếp tục: ");
+			x = sc.nextInt();
+		}
+	}
+}

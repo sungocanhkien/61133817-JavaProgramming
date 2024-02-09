ebd6086b158bd53dd39e3ced6f7bfdcd895bad0a
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-09 15:45:22 +0700

    Update BTinBangCuuChuong.java

--- HelloWorld/src/VongLapFor/BTinBangCuuChuong.java
+++ HelloWorld/src/VongLapFor/BTinBangCuuChuong.java
@@ -1,5 +1,12 @@
 package VongLapFor;
 
 public class BTinBangCuuChuong {
-
+	public static void main(String[] args) {
+		for (int i = 2; i <= 9; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i+" x "+j +" = "+(i*j)); 
+			}
+			System.out.println("---");
+		}
+	}
 }

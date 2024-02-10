f368c6aaa6cd30bc640c691f1db560436feea98c
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 21:24:30 +0700

    Lệnh break continue return
    ôn break continue return trong java

---
+++ HelloWorld/src/LenhBreakContinue/BT_Break_Continue_Return.java
@@ -0,0 +1,17 @@
+package LenhBreakContinue;
+
+public class BT_Break_Continue_Return {
+	public static void main(String[] args) {
+		int out, in = 0;
+		for (out = 0; out < 10; out++)
+		{
+			for(in = 0; in<20; in++)
+			{
+				if(in > 10)
+					break;
+			}
+			System.out.println("Bên trong vòng lặp out: "+ out + ", in = " + in);
+		}
+		System.out.println("Bên ngoài vòng lặp out: "+ out + ", in	= " + in);
+	}
+}

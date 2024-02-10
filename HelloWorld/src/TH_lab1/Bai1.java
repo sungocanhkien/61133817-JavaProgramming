1405350fa467a97877f8eeca21155847a35dfd82
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-11 00:21:22 +0700

    Update Bai1.java

--- HelloWorld/src/TH_lab1/Bai1.java
+++ HelloWorld/src/TH_lab1/Bai1.java
@@ -6,13 +6,13 @@ public class Bai1 {
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Nhập họ và tên sinh viên: ");
-        String hoTen = sc.nextLine();
-        
-        System.out.print("Nhập điểm trung bình: ");
-        double diem = sc.nextDouble();
-        
-        System.out.println(hoTen + " " + diem + " điểm");
+		
+		System.out.print("Nhập họ tên sinh viên: ");
+		String hoTen = sc.nextLine();
+		
+		System.out.print("Nhập điểm trung bình: ");
+		double diem = sc.nextDouble();
+		
+		System.out.println(hoTen + " " + diem + " điểm");
 	}
 }

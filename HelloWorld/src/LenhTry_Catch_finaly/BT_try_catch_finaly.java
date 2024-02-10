fa624d9463b686e257df42447e663df26e6e906d
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 22:20:03 +0700

    Update BT_try_catch_finaly.java
    lệnh try catch

--- HelloWorld/src/LenhTry_Catch_finaly/BT_try_catch_finaly.java
+++ HelloWorld/src/LenhTry_Catch_finaly/BT_try_catch_finaly.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class BT_try_catch_finaly {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n;
-		System.out.println("Nhập vào số nguyên n: ");
-		n = sc.nextInt();
+		int n=0;
+		try {
+			System.out.println("Nhập vào số nguyên n: ");
+			n = sc.nextInt();
+		} catch (Exception e) {
+			System.out.println("Dữ liệu nhập sai.");
+		}
 		System.out.println("Giá trị nhập là: "+ n);
 		System.out.println("Kết thúc chương trình.");
 	}

f472ec49e9ecb8e8d00e229c7422b18463797cbb
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 22:30:03 +0700

    Mạng trong java
    ôn lại mạng trong java

---
+++ HelloWorld/src/Mangjava/BTmang.java
@@ -0,0 +1,23 @@
+package Mangjava;
+
+import java.util.Scanner;
+
+public class BTmang {
+	public static void main(String[] args) {
+		double[] mang1;
+		double mang2[];
+		
+		mang1 = new double[10];
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i<mang1.length; i++)
+		{
+			System.out.println("Nhập phần tử thứ: "+i+" : ");
+			mang1[i] = sc.nextDouble();
+		}
+		double tong = 0;
+		for (int i = 0; i < mang1.length; i++) {
+			tong+= mang1[i];
+		}
+		System.out.println("Tong = "+ tong);
+	}
+}

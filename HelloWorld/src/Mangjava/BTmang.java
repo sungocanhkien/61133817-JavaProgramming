ff5f0214cc6bd478f6fd592494ce0cd87c6c385c
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 22:36:20 +0700

    Update BTmang.java

--- HelloWorld/src/Mangjava/BTmang.java
+++ HelloWorld/src/Mangjava/BTmang.java
@@ -7,18 +7,18 @@ public class BTmang {
 		double[] mang1;
 		double mang2[];
 		
-//		mang1 = new double[10];
-//		Scanner sc = new Scanner(System.in);
-//		for(int i = 0; i<mang1.length; i++)
-//		{
-//			System.out.println("Nhập phần tử thứ: "+i+" : ");
-//			mang1[i] = sc.nextDouble();
-//		}
-//		double tong = 0;
-//		for (int i = 0; i < mang1.length; i++) {
-//			tong+= mang1[i];
-//		}
-//		System.out.println("Tong = "+ tong);
+		mang1 = new double[2];
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0; i<mang1.length; i++)
+		{
+			System.out.println("Nhập phần tử thứ: "+i+" : ");
+			mang1[i] = sc.nextDouble();
+		}
+		double tong = 0;
+		for (int i = 0; i < mang1.length; i++) {
+			tong+= mang1[i];
+		}
+		System.out.println("Tong = "+ tong);
 		
 		System.out.println("Mạng 2: ");
 		mang2 = new double[]{1, 2, 3, 4, 5};

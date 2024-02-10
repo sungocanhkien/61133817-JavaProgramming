0b0c6ea23444bec27fd7b340a1a7527510392391
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 21:54:48 +0700

    Update BT_Break_Continue_Return.java

--- HelloWorld/src/LenhBreakContinue/BT_Break_Continue_Return.java
+++ HelloWorld/src/LenhBreakContinue/BT_Break_Continue_Return.java
@@ -3,12 +3,12 @@ package LenhBreakContinue;
 public class BT_Break_Continue_Return {
 	public static void main(String[] args) {
 		int out, in = 0;
-		for (out = 0; out < 10; out++)
+		outer: for (out = 0; out < 10; out++)
 		{
 			for(in = 0; in<20; in++)
 			{
 				if(in > 10)
-					break;
+					break outer;
 			}
 			System.out.println("Bên trong vòng lặp out: "+ out + ", in = " + in);
 		}

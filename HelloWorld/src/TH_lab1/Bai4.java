dafc9c98b67a8ca81eb21ecd8f5e65431e060073
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-11 00:41:07 +0700

    Thực hành lab 1 bài 4
    tính delta

--- HelloWorld/src/TH_lab1/Bai4.java
+++ HelloWorld/src/TH_lab1/Bai4.java
@@ -1,5 +1,22 @@
 package TH_lab1;
 
-public class Bai4 {
+import java.util.Scanner;
 
+public class Bai4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Nhập hệ số a: ");
+		double a = sc.nextDouble();
+		
+		System.out.println("Nhập hệ số b: ");
+		double b = sc.nextDouble();
+		
+		System.out.println("Nhập hệ số c: ");
+		double c = sc.nextDouble();
+		
+		double delTa = Math.pow(b, 2)-4*a*c;
+		
+		System.out.println("Giá trị của Delta là: "+ delTa);
+	}
 }

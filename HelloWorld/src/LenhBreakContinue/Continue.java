4e68db03ff55c3cd3d7ca0ed8c10e64932bfac58
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 22:08:27 +0700

    Update Continue.java

--- HelloWorld/src/LenhBreakContinue/Continue.java
+++ HelloWorld/src/LenhBreakContinue/Continue.java
@@ -2,13 +2,13 @@ package LenhBreakContinue;
 
 public class Continue {
 	public static void main(String[] args) {
-		int max = 100;
-		loop: for(int i=0; i<max; i++)
-		{
-			if(i<50)
-				continue loop;
-			System.out.println(i);
+		outer: for (int i = 2; i <= 9; i++) {
+			for (int j = 1; j <= 10; j++) {
+				if(i<5)
+					continue outer;
+				System.out.println(i+" x "+j +" = "+(i*j)); 
+			}
+			System.out.println("---");
 		}
-		System.out.println("End!");
 	}
 }

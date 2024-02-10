6d4dc69615d408c09eda5a62b27de41327d70b2d
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-10 22:16:09 +0700

    Lệnh try catch finaly
    ôn tập lệnh try catch finaly trong java

---
+++ HelloWorld/src/LenhTry_Catch_finaly/BT_try_catch_finaly.java
@@ -0,0 +1,14 @@
+package LenhTry_Catch_finaly;
+
+import java.util.Scanner;
+
+public class BT_try_catch_finaly {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.println("Nhập vào số nguyên n: ");
+		n = sc.nextInt();
+		System.out.println("Giá trị nhập là: "+ n);
+		System.out.println("Kết thúc chương trình.");
+	}
+}

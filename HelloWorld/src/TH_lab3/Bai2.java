dea2faa0727fc343fc940eef87021c1d1c5c8489
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-11 22:20:07 +0700

    Update Bai2.java

--- HelloWorld/src/TH_lab3/Bai2.java
+++ HelloWorld/src/TH_lab3/Bai2.java
@@ -4,11 +4,11 @@ import java.util.Iterator;
 
 public class Bai2 {
 	public static void main(String[] args) {
-		int x = 2;
-		for(int i = 1; i<=10; i++)
-		{
-			System.out.printf("%d x %d = %d", x, i, x*i);
-			System.out.println();
-		}
+		for(int i = 1; i<=9; i++)
+			for(int j = 1; j<=10; j++)
+			{
+				System.out.printf("%d x %d = %d", i, j, i*j);
+			}
+		System.out.println();
 	}
 }

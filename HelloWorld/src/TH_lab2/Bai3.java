715e53acd10e76080f90cb5331db3fdc22ee26e1
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-11 20:56:31 +0700

    Update Bai3.java

--- HelloWorld/src/TH_lab2/Bai3.java
+++ HelloWorld/src/TH_lab2/Bai3.java
@@ -1,5 +1,22 @@
 package TH_lab2;
 
-public class Bai3 {
+import java.util.Scanner;
 
+public class Bai3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Nhập vào số điện của tháng: ");
+		int soDien = sc.nextInt();
+		int tien;
+		
+		if(soDien <= 50)
+		{
+			tien = soDien*1000;
+		}else
+		{
+			tien = 50*1000+(soDien-50)*1200;
+		}
+		System.out.println("Tiền điện phải trả là: "+ tien + "đồng");
+	}
 }

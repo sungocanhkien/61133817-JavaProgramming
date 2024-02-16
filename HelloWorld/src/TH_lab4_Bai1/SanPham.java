0a5ac6b5817dccd95c0ac72d0cee16c915630d4c
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-16 14:50:47 +0700

    update

--- HelloWorld/src/TH_lab4_Bai1/SanPham.java
+++ HelloWorld/src/TH_lab4_Bai1/SanPham.java
@@ -1,5 +1,33 @@
 package TH_lab4_Bai1;
 
-public class SanPham {
+import java.util.Scanner;
 
+public class SanPham {
+	private String tenSp;
+	private double donGia;
+	private double giamGia;
+	
+	public double getThueNhapKhau()
+	{
+		return 0.1 * donGia;
+	}
+	
+	public void xuat()
+	{
+		System.out.println("Tên sản phẩm: " + tenSp);
+		System.out.println("Giá: " + donGia);
+		System.out.println("Giảm giá: "+ giamGia);
+		System.out.println("Thuế nhập khẩu: "+ getThueNhapKhau());
+	}
+	
+	public void nhap()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập tên sản phẩm: ");
+		tenSp = sc.nextLine();
+		System.out.println("Nhập giá sản phẩm: ");
+		donGia = sc.nextDouble();
+		System.out.println("Nhập giảm giá: ");
+		giamGia = sc.nextDouble();
+	}
 }

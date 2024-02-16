96961affca5eaaec8599b5bb84d1f417ab1e1011
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-16 14:05:11 +0700

    update

--- HelloWorld/src/Get_Set_trong_HuongDoiTuong/BT_mau_MyDate.java
+++ HelloWorld/src/Get_Set_trong_HuongDoiTuong/BT_mau_MyDate.java
@@ -1,5 +1,31 @@
 package Get_Set_trong_HuongDoiTuong;
 
 public class BT_mau_MyDate {
-
+	private int day;
+	private int month;
+	private int year;
+	
+	public BT_mau_MyDate(int day, int month, int year) {
+		if(day >= 1 && day <= 31)
+		{
+			this.day = day;
+		}else {
+			this.day = 1;
+		}
+		if(month >= 1 && month <= 12) {
+			this.month = month;
+		}else {
+			month = 1;
+		}
+		if(year >= 0) {
+			this.year = year;
+		}else {
+			year = 1;
+		}
+	}
+	
+	public int getDay() {
+		return this.day;
+	}
+	
 }
--- HelloWorld/src/Get_Set_trong_HuongDoiTuong/Main_Get_Set.java
+++ HelloWorld/src/Get_Set_trong_HuongDoiTuong/Main_Get_Set.java
@@ -1,5 +1,9 @@
 package Get_Set_trong_HuongDoiTuong;
 
 public class Main_Get_Set {
-
+	public static void main(String[] args) {
+		BT_mau_MyDate md = new BT_mau_MyDate(16, 2, 2024);
+		
+		System.out.println("Day: " + md.getDay());
+	}
 }

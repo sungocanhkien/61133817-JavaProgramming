9553eb1eb1679680367ccc583c2be356500299b6
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-16 15:28:06 +0700

    Update Main_Bai2.java

--- HelloWorld/src/TH_lab4/Main_Bai2.java
+++ HelloWorld/src/TH_lab4/Main_Bai2.java
@@ -4,9 +4,15 @@ public class Main_Bai2 {
 	public static void main(String[] args) {
 		SanPham_Bai1 sp1 = new SanPham_Bai1();
 		SanPham_Bai1 sp2 = new SanPham_Bai1();
+		
+		System.out.println("Nhập thông tin sản phẩm 1: ");
 		sp1.nhap();
+		System.out.println("Nhập thông tin sản phẩm 2: ");
 		sp2.nhap();
+		
+		System.out.println("Thông tin sản phẩm 1: ");
 		sp1.xuat();
+		System.out.println("Thông tin sản phẩm 2: ");
 		sp2.xuat();
 		
 	}

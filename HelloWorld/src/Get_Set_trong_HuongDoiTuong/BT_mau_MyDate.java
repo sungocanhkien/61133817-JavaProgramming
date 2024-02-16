4590fbc9aee9c5a83fbcfd8feb11dffc5080c74f
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-16 14:17:08 +0700

    update

--- HelloWorld/src/Get_Set_trong_HuongDoiTuong/BT_mau_MyDate.java
+++ HelloWorld/src/Get_Set_trong_HuongDoiTuong/BT_mau_MyDate.java
@@ -27,5 +27,30 @@ public class BT_mau_MyDate {
 	public int getDay() {
 		return this.day;
 	}
-	
+	public void setDay(int d) {
+		if(d >= 1 && d <= 31 )
+			this.day = d;
+	}
+
+	public int getMonth() {
+		return this.month;
+	}
+
+	public void setMonth(int month) {
+		if(month >= 1 && month <= 12)
+		{
+			this.month = month;
+		}	
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		if(year >=  1)
+		{
+			this.year = year;
+		}
+	}
 }
--- HelloWorld/src/Get_Set_trong_HuongDoiTuong/Main_Get_Set.java
+++ HelloWorld/src/Get_Set_trong_HuongDoiTuong/Main_Get_Set.java
@@ -5,5 +5,16 @@ public class Main_Get_Set {
 		BT_mau_MyDate md = new BT_mau_MyDate(16, 2, 2024);
 		
 		System.out.println("Day: " + md.getDay());
+		
+		md.setDay(35);
+		System.out.println("Day: " + md.getDay());
+		md.setDay(30);
+		System.out.println("Day: " + md.getDay());
+		
+		System.out.println("Month: " + md.getMonth());
+		md.setMonth(13);
+		System.out.println("Month: " + md.getMonth());
+		md.setMonth(5);
+		System.out.println("Month: " + md.getMonth());
 	}
 }

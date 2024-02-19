e3b056fe3b173ac6651025270001c2f78956101e
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-20 00:25:20 +0700

    Update Bai3_Class_Main.java

--- HelloWorld/src/TH_lab5/Bai3_Class_Main.java
+++ HelloWorld/src/TH_lab5/Bai3_Class_Main.java
@@ -2,6 +2,7 @@ package TH_lab5;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Bai3_Class_Main {
 	public static void main(String[] args) {
@@ -10,9 +11,21 @@ public class Bai3_Class_Main {
 		int luaChon;
 		
 		do{
+			System.out.println("--MENU--");
+			System.out.println("1. Nhập danh sách sản phẩm từ bàn phím.");
+			System.out.println("2. Sắp xếp giảm dần theo giá và xuất ra màn hình.");
+			System.out.println("3. Tìm và xóa sản phẩm theo tên nhập từ bàn phím.");
+			System.out.println("4. Xuất giá trung bình của các sản phẩm.");
+			luaChon = sc.nextInt();
 			
-			
-			
+			switch (key) {
+			case value: {
+				
+				yield type;
+			}
+			default:
+				w IllegalArgumentException("Unexpected value: " + key);
+			}
 		}while(luaChon != 5);
 	}
 }

8aa4889bead8ae16f1da60f26ef4e997b28e7ed9
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-20 00:17:47 +0700

    Thực hành lab5 bài 3

---
+++ HelloWorld/src/TH_lab5/Bai3_Class_Main.java
@@ -0,0 +1,6 @@
+package TH_lab5;
+
+public class Bai3_Class_Main {
+	
+
+}
---
+++ HelloWorld/src/TH_lab5/Bai3_Class_SanPham.java
@@ -0,0 +1,12 @@
+package TH_lab5;
+
+public class Bai3_Class_SanPham {
+	String ten;
+	double gia;
+	
+	public void SanPham(String ten, double gia)
+	{
+		this.ten = ten;
+		this.gia = gia;
+	}
+}

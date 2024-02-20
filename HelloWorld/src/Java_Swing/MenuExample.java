036f2f11bb39b0bcc4d1795505d22698c31488ad
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-20 15:07:22 +0700

    Update MenuExample.java

--- HelloWorld/src/Java_Swing/MenuExample.java
+++ HelloWorld/src/Java_Swing/MenuExample.java
@@ -8,5 +8,14 @@ public class MenuExample {
         JMenuBar menuBar = new JMenuBar(); // Tạo một menu bar JMenuBar
         JMenu fileMenu = new JMenu("File"); // Tạo một menu "File" JMenu
         
+        JMenuItem newItem = new JMenuItem("New"); // Tạo một JMenuItem "New"
+        JMenuItem openItem = new JMenuItem("Open"); // Tạo một JMenuItem "Open"
+        JMenuItem saveItem = new JMenuItem("Save"); // Tạo một JMenuItem "Save"
+        JMenuItem exitItem = new JMenuItem("Exit"); // Tạo một JMenuItem "Exit"
+        
+        fileMenu.add(newItem); // Thêm mục "New" vào menu "File"
+        fileMenu.add(openItem); // Thêm mục "Open" vào menu "File"
+        fileMenu.add(saveItem); // Thêm mục "Save" vào menu "File"
+        fileMenu.add(exitItem); // Thêm mục "Exit" vào menu "File"
 	}
 }

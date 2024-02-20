ecdaf33e6cb766c6d1d2e248a9f7d35fc9e85928
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-20 15:01:15 +0700

    Update MenuExample.java

--- HelloWorld/src/Java_Swing/MenuExample.java
+++ HelloWorld/src/Java_Swing/MenuExample.java
@@ -4,6 +4,9 @@ import java.awt.event.*;
 
 public class MenuExample {
 	public static void main(String[] args) {
-		
+		JFrame frame = new JFrame("Menu Example"); // Tạo một cửa sổ JFrame với tiêu đề "Menu Example"
+        JMenuBar menuBar = new JMenuBar(); // Tạo một menu bar JMenuBar
+        JMenu fileMenu = new JMenu("File"); // Tạo một menu "File" JMenu
+        
 	}
 }

7ddb850e87d3b0fe290f12f37ff466bd1def1933
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-02-20 14:37:27 +0700

    Tìm hiểu java Swing

---
+++ HelloWorld/src/Java_Swing/Button.java
@@ -0,0 +1,18 @@
+package Java_Swing;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+public class Button {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();// tạo thể hiện của JFrame
+		 
+        JButton b = new JButton("click");// tạo thể hiện của JButton
+        b.setBounds(130, 50, 100, 40);// trục x , y , width, height
+ 
+        f.setTitle("Ví dụ Java Swing");
+        f.add(b);// thêm button vào JFrame
+ 
+        f.setSize(400, 200);// thiết lập kích thước cho của sổ
+        f.setLayout(null);// không sử dụng trình quản lý bố cục
+        f.setVisible(true);// hiển thị cửa sổ
+	}
+}

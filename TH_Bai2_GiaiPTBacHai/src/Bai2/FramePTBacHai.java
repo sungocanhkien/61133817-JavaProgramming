e81868526263f007fd547687834a7c9b5c9cb6cb
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-04 16:59:22 +0700

    Update FramePTBacHai.java

--- TH_Bai2_GiaiPTBacHai/src/Bai2/FramePTBacHai.java
+++ TH_Bai2_GiaiPTBacHai/src/Bai2/FramePTBacHai.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class FramePTBacHai extends JFrame {
 
@@ -36,11 +38,27 @@ public class FramePTBacHai extends JFrame {
 		setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		setTitle("GIẢI PHƯƠNG TRÌNH BẬC HAI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 692, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nhập số a:");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblNewLabel.setBounds(44, 49, 77, 28);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNhpSB = new JLabel("Nhập số b:");
+		lblNhpSB.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblNhpSB.setBounds(44, 87, 77, 28);
+		contentPane.add(lblNhpSB);
+		
+		JLabel lblCnhpSA = new JLabel("Nhập số c:");
+		lblCnhpSA.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblCnhpSA.setBounds(44, 125, 77, 28);
+		contentPane.add(lblCnhpSA);
 	}
 
 }

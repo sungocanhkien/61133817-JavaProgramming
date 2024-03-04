ddce74912dc5f19eeb203b89fd6427d3868ebca6
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-04 15:59:05 +0700

    TH_Bai2_JavaSwing_JFrame

---
+++ TH_Bai1_Login/src/Bai1/HomeFrame.java
@@ -0,0 +1,28 @@
+package Bai1;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class HomeFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public HomeFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
---
+++ TH_Bai1_Login/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module TH_Bai1_Login {
+	requires java.desktop;
+}

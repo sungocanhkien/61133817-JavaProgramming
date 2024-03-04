9ddad98a5a041cf7c55bdb7a1c1e3e43d15087ef
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-04 16:05:57 +0700

    updates

--- TH_Bai1_Login/src/Bai1/ChuongTrinhChinh.java
+++ TH_Bai1_Login/src/Bai1/ChuongTrinhChinh.java
@@ -1,5 +1,7 @@
 package Bai1;
 
 public class ChuongTrinhChinh {
-
+	public static void main(String[] args) {
+		new LoginJFrame().setVisible(true);
+	}
 }
--- TH_Bai1_Login/src/Bai1/LoginJFrame.java
+++ TH_Bai1_Login/src/Bai1/LoginJFrame.java
@@ -5,11 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class LoginJFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -31,12 +36,37 @@ public class LoginJFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public LoginJFrame() {
+		setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		setTitle("ĐĂNG NHẬP HỆ THỐNG");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Tên đăng nhập:");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblNewLabel.setBounds(37, 41, 106, 31);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblMtKhu = new JLabel("Mật khẩu:");
+		lblMtKhu.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblMtKhu.setBounds(37, 76, 106, 31);
+		contentPane.add(lblMtKhu);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		textField.setBounds(135, 48, 167, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		textField_1.setColumns(10);
+		textField_1.setBounds(135, 82, 167, 19);
+		contentPane.add(textField_1);
 	}
 
 }

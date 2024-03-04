336e85751bd98be47a7fe76eea15315ab899d77c
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-04 15:07:58 +0700

    Update ManHinhTinhToan.java

--- TH_Bai0_SimpleCalculator/src/Bai0/ManHinhTinhToan.java
+++ TH_Bai0_SimpleCalculator/src/Bai0/ManHinhTinhToan.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import javax.swing.JLabel;
 
 public class ManHinhTinhToan extends JFrame {
 
@@ -31,12 +33,19 @@ public class ManHinhTinhToan extends JFrame {
 	 * Create the frame.
 	 */
 	public ManHinhTinhToan() {
+		setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		setTitle("Chương Trình Tính Toán");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 820, 508);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nhập số thứ nhất (a):");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblNewLabel.setBounds(44, 45, 143, 25);
+		contentPane.add(lblNewLabel);
 	}
-
 }

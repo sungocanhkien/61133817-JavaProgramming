ad035e49c60f72f3eb4a0d540d33942bf9f976f2
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2024-03-04 15:17:58 +0700

    Update ManHinhTinhToan.java

--- TH_Bai0_SimpleCalculator/src/Bai0/ManHinhTinhToan.java
+++ TH_Bai0_SimpleCalculator/src/Bai0/ManHinhTinhToan.java
@@ -7,11 +7,18 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ManHinhTinhToan extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -52,5 +59,49 @@ public class ManHinhTinhToan extends JFrame {
 		lblNhpSTh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
 		lblNhpSTh.setBounds(44, 101, 143, 25);
 		contentPane.add(lblNhpSTh);
+		
+		textField = new JTextField();
+		textField.setBounds(183, 49, 212, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(183, 105, 212, 19);
+		contentPane.add(textField_1);
+		
+		JButton btnNewButton = new JButton("Cộng");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		btnNewButton.setBounds(81, 186, 85, 21);
+		contentPane.add(btnNewButton);
+		
+		JButton btnTr = new JButton("Trừ");
+		btnTr.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		btnTr.setBounds(194, 187, 85, 21);
+		contentPane.add(btnTr);
+		
+		JButton btnNhn = new JButton("Nhân");
+		btnNhn.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		btnNhn.setBounds(304, 187, 85, 21);
+		contentPane.add(btnNhn);
+		
+		JButton btnChia = new JButton("Chia");
+		btnChia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnChia.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		btnChia.setBounds(428, 187, 85, 21);
+		contentPane.add(btnChia);
+		
+		JLabel lblKtQuTnh = new JLabel("Kết quả tính toán:");
+		lblKtQuTnh.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+		lblKtQuTnh.setBounds(44, 258, 143, 25);
+		contentPane.add(lblKtQuTnh);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(167, 262, 212, 19);
+		contentPane.add(textField_2);
 	}
 }

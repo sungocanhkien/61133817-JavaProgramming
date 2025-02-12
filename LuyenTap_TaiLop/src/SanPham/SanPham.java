a1efb6daf0806a0970cd97be3589cb2f52aff1fb
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-12 17:14:49 +0700

    Update SanPham.java

--- LuyenTap_TaiLop/src/SanPham/SanPham.java
+++ LuyenTap_TaiLop/src/SanPham/SanPham.java
@@ -13,7 +13,13 @@ public class SanPham {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Dinh nghia cac thuoc tinh
+		int maSP;
+		String tenSP;
+		String loaiSP;
+		String anhSP;
+		
 	}
 
+	
 }

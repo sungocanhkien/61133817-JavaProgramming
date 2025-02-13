c6ebd6e1e552ab77c4d50d9a2644e1b6cc517783
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-13 21:31:35 +0700

    Update SanPham.java
    Thêm code

--- vidu_baitap_HuongDoiTuong/src/vidu_baitap_HuongDoiTuong/SanPham.java
+++ vidu_baitap_HuongDoiTuong/src/vidu_baitap_HuongDoiTuong/SanPham.java
@@ -1,5 +1,45 @@
 package vidu_baitap_HuongDoiTuong;
 
 public class SanPham {
-
+	int maSP;
+	String tenSP;
+	String loaiSP;
+	String anhSP;
+	public SanPham(int maSP, String tenSP, String loaiSP, String anhSP) {
+		super();
+		this.maSP = maSP;
+		this.tenSP = tenSP;
+		this.loaiSP = loaiSP;
+		this.anhSP = anhSP;
+	}
+	public int getMaSP() {
+		return maSP;
+	}
+	public void setMaSP(int maSP) {
+		this.maSP = maSP;
+	}
+	public String getTenSP() {
+		return tenSP;
+	}
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+	public String getAnhSP() {
+		return anhSP;
+	}
+	public void setAnhSP(String anhSP) {
+		this.anhSP = anhSP;
+	}
+	@Override
+	public String toString() {
+		return "[Mã SP=" + maSP + ", Tên SP=" + tenSP + ", Loại SP=" + loaiSP + "]";
+	}
+	
+	
 }

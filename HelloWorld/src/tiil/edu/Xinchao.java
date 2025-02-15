ce14f5a7856f640a6421fded03cdb2960738f514
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 06:54:31 +0700

    Update Xinchao.java
    thêm code

--- HelloWorld/src/tiil/edu/Xinchao.java
+++ HelloWorld/src/tiil/edu/Xinchao.java
@@ -14,6 +14,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Xin chào!");
+		System.out.println("My Name is Kien");
 
 	}
 

1842843562d64387542905e2224d9ccd78e60683
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 14:55:08 +0700

    Update TheTich.java

--- Lab1_Bai3_TheTich/src/TheTich.java
+++ Lab1_Bai3_TheTich/src/TheTich.java
@@ -1,15 +1,19 @@
-/**
- * 
- */
+import java.util.Scanner;
 
-/**
- * 
- */
 public class TheTich {
 
 	public static void main(String[] args) {
+		//Khai báo Scanner
+		Scanner scanner = new Scanner(System.in);
+		//Nhập vào cạnh của lập phương
+		System.out.print("Nhập cạnh của khối lập phương: ");
+		double canh = scanner.nextDouble();
+		//Tính thể tỉnh khối lập phương
+		double theTich = Math.pow(canh, 3);
 		
-
+		//Xuất kết quả
+		System.out.println("Thể tích khối lập phương là: ");
+		
 	}
 
 }

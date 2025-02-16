454c5a9d6e68c3199fec4fdd019df7540d1f73e3
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 15:39:58 +0700

    Update DoiTuong_Oto.java

--- Vidu_ArrayList/src/DoiTuong_Oto.java
+++ Vidu_ArrayList/src/DoiTuong_Oto.java
@@ -4,7 +4,14 @@ public class DoiTuong_Oto {
 	String tenOto;
 	String mauSacOto;
 	int namSanXuatOto;
-	
+	//Constructor 
+	public DoiTuong_Oto(int maOto, String tenOto, String mauSacOto, int namSanXuatOto) {
+		super();
+		this.maOto = maOto;
+		this.tenOto = tenOto;
+		this.mauSacOto = mauSacOto;
+		this.namSanXuatOto = namSanXuatOto;
+	}
 	
 	
 }

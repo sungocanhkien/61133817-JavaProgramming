39029dbf42d0a6a25fed361cb0930da654046171
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 07:06:40 +0700

    Update CT.java

--- vidu_baitap_HuongDoiTuong/src/vidu_baitap_HuongDoiTuong/CT.java
+++ vidu_baitap_HuongDoiTuong/src/vidu_baitap_HuongDoiTuong/CT.java
@@ -9,6 +9,13 @@ public class CT {
 		sp1.setTenSP("Dây Tây");
 		sp1.setLoaiSP("Trái Cây");
 		sp1.setAnhSP(null);
+		
+		//In ra màn hình thông tin 2 sp
+		String thongtinSP1 = sp1.toString();
+		String thongtinSP2 = "Mã SP2 là" + sp2.getMaSP();
+	    	   thongtinSP2 = "Tên SP2 là" + sp2.getTenSP();
+	    System.out.print(thongtinSP1);
+	    
 
 	}
 

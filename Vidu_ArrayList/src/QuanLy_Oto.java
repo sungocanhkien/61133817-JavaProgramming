54d225f8a8970ee7f8ff306c133c885fd3db027e
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 15:55:43 +0700

    Create QuanLy_Oto.java

---
+++ Vidu_ArrayList/src/QuanLy_Oto.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class QuanLy_Oto {
+
+	public static void main(String[] args) {
+		//Khởi tạo danh sách để lưu trữ 
+		ArrayList<DoiTuong_Oto> DS_Oto = new ArrayList<>();
+		
+
+	}
+
+}

6483eaa4a3e2cc990a0d8a093cb1380d485cf166
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 09:53:18 +0700

    Update DTB.java

--- Lab1_Bai1_DTB/src/lab1bai1/edu/DTB.java
+++ Lab1_Bai1_DTB/src/lab1bai1/edu/DTB.java
@@ -3,13 +3,19 @@
  */
 package lab1bai1.edu;
 
+import java.util.Scanner;
+
 /**
  * 
  */
 public class DTB {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//Tạo đối tượng Scanner để nhập dữ liệu từ bàn phím
+		Scanner scanner = new Scanner(System.in);
+		//Nhập họ và tên
+		System.out.print("Họ và tên: ");
+		Stri
 
 	}
 

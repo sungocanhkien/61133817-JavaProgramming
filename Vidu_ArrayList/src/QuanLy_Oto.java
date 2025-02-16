1d43e0ae6fd754bea822c29db81ca98640b4defc
sungocanhkien <156717204+sungocanhkien@users.noreply.github.com>
2025-02-16 16:00:25 +0700

    Update QuanLy_Oto.java

--- Vidu_ArrayList/src/QuanLy_Oto.java
+++ Vidu_ArrayList/src/QuanLy_Oto.java
@@ -5,6 +5,10 @@ public class QuanLy_Oto {
 		//Khởi tạo danh sách để lưu trữ 
 		ArrayList<DoiTuong_Oto> DS_Oto = new ArrayList<>();
 		
+		//Thêm các đối tượng ô tô vào danh sách
+		DS_Oto.add(new DoiTuong_Oto(0, "Toyota", "Cam", 2022));
+		DS_Oto.add(new DoiTuong_Oto(1, "Honda", "Đỏ", 2023));
+		DS_
 
 	}
 

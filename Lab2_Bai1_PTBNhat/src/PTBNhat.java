90cd718b90753cc3803185f7cfdefba24ea73b00
sungocanhkien <kiensu95@gmail.com>
2025-02-22 18:57:15 +0700

    Update PTBNhat.java

--- Lab2_Bai1_PTBNhat/src/PTBNhat.java
+++ Lab2_Bai1_PTBNhat/src/PTBNhat.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 
 public class PTBNhat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Khởi tạo đối tượng Scanner
+		Scanner scanner = new Scanner(System.in);
+		//Nhập hệ số a
+		System.out.print("Nhập hệ số a: ");
+		double a = scanner.nextDouble();
 		
-
+		//Nhập hệ số b
+		System.out.print("Nhập hệ số b: ");
+		double b = scanner.nextDouble();
+		
+		//Kiểm tra các trường hợp của phương trình
+		if(a == 0) {
+			if(b == 0) {
+				//Trường hợp a = 0 và b = 0, PT vô số nghiệm
+				System.out.println
+			}
+		}
 	}
 
 }

1d470e6a4400b76d319b7cbd196acfd10e54a1ba
sungocanhkien <kiensu95@gmail.com>
2025-02-23 00:14:40 +0700

    Update Main data cho Lab2_Bai2

--- Lab4_Bai1234_Lop_DoiTuong/src/Main.java
+++ Lab4_Bai1234_Lop_DoiTuong/src/Main.java
@@ -3,9 +3,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SanPham sp = new SanPham();
-		//sp.nhap();
-		sp.xuat();
+		//Tạo 2 đối tượng sản phẩm
+		SanPham sp1 = new SanPham();
+		SanPham sp2 = new SanPham();
+		
+		//Nhập thông tin cho từng sản phẩm
+		System.out.println("Nhập thông tin sản phẩm 1:");
+		sp1.nhap();
+		System.out.println("Nhập thông tin sản phẩm 2:");
+		sp2.nhap();
+		
+		
 	}
 
 }

7b7d5afbf990affd598e3041c78c10def181b077
sungocanhkien <kiensu95@gmail.com>
2025-02-22 18:59:34 +0700

    Update PTBNhat.java

--- Lab2_Bai1_PTBNhat/src/PTBNhat.java
+++ Lab2_Bai1_PTBNhat/src/PTBNhat.java
@@ -18,7 +18,10 @@ public class PTBNhat {
 		if(a == 0) {
 			if(b == 0) {
 				//Trường hợp a = 0 và b = 0, PT vô số nghiệm
-				System.out.println
+				System.out.println("PT vô số nghiệm!");
+			} else {
+				//Trường hợp a = 0 và b != 0, vô nghiệm
+				System.out.prinln("PT vô nghiệm!");
 			}
 		}
 	}

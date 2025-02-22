4cf7fe856a75d1da52afd02456ec8213e390361c
sungocanhkien <kiensu95@gmail.com>
2025-02-22 19:20:24 +0700

    Update SanPham.java

--- Lab4_Bai1234_Lop_DoiTuong/src/SanPham.java
+++ Lab4_Bai1234_Lop_DoiTuong/src/SanPham.java
@@ -4,5 +4,13 @@ public class SanPham {
 	private String tensp;
 	private double donGia;
 	private double giamGia;
+	//Hàm tạo 3 tham số
+	public SanPham(String tensp, double donGia, double giamGia) {
+		super();
+		this.tensp = tensp;
+		this.donGia = donGia;
+		this.giamGia = giamGia;
+	}
+	
 	
 }
